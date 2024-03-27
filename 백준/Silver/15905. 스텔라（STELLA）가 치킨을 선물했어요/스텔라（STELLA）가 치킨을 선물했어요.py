50a7724166dcabc05d14c8940f7b3230e3b8e935
YJK-dev <90750483+YJK-dev@users.noreply.github.com>
2024-03-27 22:49:43 +0900

    [Silver V] Title: 스텔라(STELLA)가 치킨을 선물했어요, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15905. 스텔라（STELLA）가 치킨을 선물했어요/스텔라（STELLA）가 치킨을 선물했어요.py
+++ 백준/Silver/15905. 스텔라（STELLA）가 치킨을 선물했어요/스텔라（STELLA）가 치킨을 선물했어요.py
@@ -3,13 +3,13 @@ data = []
 answer = 0
 
 for _ in range(n):
-    q_cnt, s_cnt = map(int, input().split())
-    data.append((q_cnt, s_cnt))
+    data.append(list(map(int, input().split())))
 
-data.sort(key=lambda x: (x[0], x[1]), reverse = True)
+data.sort(key=lambda x: (-x[0], x[1]))
+fifth = data[4][0]
 
 for i in range(5, n):
-    if data[4][0] == data[i][0] and data[4][1] > data[i][1]:
+    if data[i][0] == fifth:
         answer += 1
 
 print(answer)
