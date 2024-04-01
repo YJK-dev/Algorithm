f89aac2beb2e88b8551d4983981f7ecc3d78fe5f
YJK-dev <90750483+YJK-dev@users.noreply.github.com>
2024-04-01 11:14:30 +0900

    [Silver I] Title: 포도주 시식, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,16 +1,17 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
 wine = [0] * 10001
-summ = [0] * 10001
+dy = [0] * 10001
 
 for i in range(1, n+1):
     wine[i] = int(input())
     
-summ[1] = wine[1]
-
-if n>=2:
-    summ[2] = max(wine[1], 0) + wine[2]
+dy[1] = wine[1]
+dy[2] = wine[1] + wine[2]
 
 for i in range(3, n+1):
-    summ[i] = max(summ[i-1], wine[i] + wine[i-1] + summ[i-3], wine[i] + summ[i-2])
+    dy[i] = max(dy[i-1], wine[i]+wine[i-1]+dy[i-3], wine[i]+dy[i-2])
     
-print(summ[n])
+print(dy[n])
