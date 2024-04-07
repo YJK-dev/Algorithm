76d245f2f04724d3c08b58a13487632652246a2a
YJK-dev <90750483+YJK-dev@users.noreply.github.com>
2024-04-07 16:07:08 +0900

    [Silver III] Title: 게임, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+x, y = map(int, input().split()) # 게임 횟수, 이긴 게임
+z = y * 100 // x # 승률 계산
+
+if z >= 99:
+    print(-1)
+else:
+    answer = 0
+    start = 1
+    end = x
+
+    while start <= end:
+        mid = (start + end) // 2
+        percent = (y+mid) * 100 // (x+mid)
+
+        if z >= percent:
+            start = mid + 1
+        else:
+            answer = mid
+            end = mid - 1
+    
+    print(answer)
